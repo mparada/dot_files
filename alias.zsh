08809efe9d36f3adeaa80896ab3b31b6d37d8449
Marcelo Parada <marcelo.ito.parada@gmail.com>
2018-08-23 12:05:51 +0200

    Use my versions of cookiecutter

--- alias.zsh
+++ alias.zsh
@@ -15,7 +15,8 @@ alias tkill='tmux kill-session -t'
 alias dbcon='pgcli -d D9SBX -p 5438 -h axonlu-dbsvr5.mappuls.int -U mpr'
 
 # Data Science project template: cookiecutter
-alias cookiecutter='cookiecutter https://github.com/drivendata/cookiecutter-data-science'
+alias makecookie='cookiecutter https://github.com/mparada/cookiecutter-data-science'
+alias makecookiesimple='cookiecutter https://github.com/mparada/cookiecutter-data-science-simplified'
 
 # SSH tunnel to GPU Server's RStudio
 alias gpu-rstudio='ssh -N -p 22000 -i ~/.ssh/id_aapmapa aapmapa@194.247.8.74 -L 8787:127.0.0.1:8787'
